6457f2b6e14834387ce2664d44464c5d2a13602d
shah <faisalkhanshah@gmail.com>
2019-06-24 10:39:32 +0000

    tomcat implemented

--- site.pp
+++ site.pp
@@ -1,21 +1,9 @@
-node 'default' {
-class { 'apache':                # use the "apache" module
-    default_vhost => false,        # don't use the default vhost
-    default_mods => false,         # don't load default mods
-    mpm_module => 'prefork',        # use the "prefork" mpm_module
-  }
-   include apache::mod::php        # include mod php
-   apache::vhost { 'example.com':  # create a vhost called "example.com"
-    port    => '80',               # use port 80
-    docroot => '/var/www/html',     # set the docroot to the /var/www/html
-  }
-class { 'mysql::server':
-    root_password => 'root',
-  }
-file { 'info.php':                                # file resource name
-    path => '/var/www/html/info.php',               # destination path
-    ensure => file,
-    require => Class['apache'],                     # require apache class be used
-    source => 'puppet:///modules/apache/info.php',  # specify location of file to be copied
-  }
+class {'java':}
+class {'tomcat':}
+tomcat::install { '/opt/tomcat':
+ # source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.x/bin/apache-tomcat-7.0.x.tar.gz',
+  source_url => 'http://mirrors.estointernet.in/apache/tomcat/tomcat-9/v9.0.21/bin/apache-tomcat-9.0.21.tar.gz',
+}
+tomcat::instance { 'default':
+  catalina_home => '/opt/tomcat',
 }
